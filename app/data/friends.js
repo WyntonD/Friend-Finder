34ebaed177c45f216eed5db6c135b958acf2375b
WD$ <wyntonmdavis@gmail.com>
2019-10-19 17:03:52 -0400

    initial json parse

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,8 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
-
-var PORT = process.env.PORT || 8070;
-
-
-app.listen(PORT, function() {
-    console.log("Server listening on : http://localhost:" + PORT);
-})
 
 var people = {
     name:"Ahmed",
@@ -25,5 +19,44 @@ var people = {
         4,
         1
       ]
-  }
-  
+    
+};
+
+app.get("/", function(req, res) {
+    res.send("Hello World")
+})
+
+app.get("/people", function(req, res) {
+   res.json(people);
+    // var chosen = req.params.people;
+
+//    for (var i = 0; i < people.length; i++){
+//     if (chosen === people[i].name){
+//         res.json(people[i]);
+//         return;
+//     }
+//   res.send("This person is not in our database");
+//    }
+
+})
+
+app.listen(3000);
+
+var PORT = process.env.PORT || 8070;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+
+app.listen(PORT, function() {
+    console.log("Server listening on : http://localhost:" + PORT);
+})
+
